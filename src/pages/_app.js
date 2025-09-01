40e13ad76c95719434b2c1ca83b434bdae43de0b
sunny-zerror <sunny.zerror@gmail.com>
2025-09-01 11:45:06 +0530

    project setup

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+import Layout from "@/components/Layout";
+import LenisScroll from "@/components/LenisScroll";
+import "@/styles/globals.css";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function App({ Component, pageProps }) {
+  return (
+    <LenisScroll>
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+    </LenisScroll>
+  );
+}
