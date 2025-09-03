d3cfb122eae8550ad7efeefd9fadae869a327a4a
anand-zerror <anand.zerror@gmail.com>
2025-09-04 01:04:35 +0530

    ON Page SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: false,
   reactStrictMode: true,
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/api/sitemaps/sitemap_index",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const Custom404 = () => {
-    return (
-        <div>
-            <div className="w-full h-screen center flex-col gap-2">
-                <p className='text-6xl'>Page Not Found</p>
-                <p className='text-xl'>Sorry, the page you are looking for does not exist.</p>
-                <a href="/">
-                    <p className='underline'>Go Back</p>
-                </a>
-            </div>
+const Custom404 = ({ meta }) => {
+  return (
+    <>
+      <SeoHeader meta={meta} />
+      <div>
+        <div className="w-full h-screen center flex-col gap-2">
+          <p className="text-6xl">Page Not Found</p>
+          <p className="text-xl">
+            Sorry, the page you are looking for does not exist.
+          </p>
+          <a href="/">
+            <p className="underline">Go Back</p>
+          </a>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default Custom404
+export default Custom404;
+
+export async function getStaticProps() {
+  const meta = {
+    title: "404 Page Not Found | SHIVDUTT DAS ART FOUNDATION (SDAF)",
+    description:
+      "The page you are looking for cannot be found. Return to SHIVDUTT DAS ART FOUNDATION (SDAF) to explore exhibitions and artworks.",
+    keywords: "",
+    og: {
+      title: "404 Page Not Found | SHIVDUTT DAS ART FOUNDATION (SDAF)",
+      description:
+        "The page you are looking for cannot be found. Return to SHIVDUTT DAS ART FOUNDATION (SDAF) to explore exhibitions and artworks.",
+    },
+    twitter: {
+      title: "404 Page Not Found | SHIVDUTT DAS ART FOUNDATION (SDAF)",
+      description:
+        "The page you are looking for cannot be found. Return to SHIVDUTT DAS ART FOUNDATION (SDAF) to explore exhibitions and artworks.",
+    },
+    robots: "noindex,nofollow",
+  };
+  return {
+    props: {
+      meta,
+    },
+  };
+}
